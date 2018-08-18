2cd67ec7b0d8523d830e006c7a3ad0fdf471c54c
Fatih Melih Özgün <34399010+fedqx@users.noreply.github.com>
2018-08-18 11:29:01 +0300

    Update Texture.cpp

--- Texture.cpp
+++ Texture.cpp
@@ -3,8 +3,19 @@
 // Copyright (c) 2015-2016 Game Institute. All Rights Reserved.
 //
 // Simple 2D texture class
-// Modified Texture.cpp by Fatih Melih �ZG�N 
+// Modified Texture.cpp by Fatih Melih ÖZGÜN 
 //-----------------------------------------------------------------------------
+/*Copyright (C) 2018  Mehmet ÖZGÜN
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include "Texture.h"
 #include <iostream>
 #include <cassert>
