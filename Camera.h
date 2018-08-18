64f9b35ffd7755798cca3d3494031edd9dfeb7e2
Fatih Melih Özgün <34399010+fedqx@users.noreply.github.com>
2018-08-18 11:24:36 +0300

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -4,8 +4,22 @@
 //
 // Basic camera class including derived orbit-style and first person
 // shooter (FPS) style camera support
-// Modified Camera.h by Fatih Melih �ZG�N 
+// Modified Camera.h by Fatih Melih ÖZGÜN 
 //-----------------------------------------------------------------------------
+/*Copyright (C) 2018  Mehmet ÖZGÜN
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #ifndef CAMERA_H
 #define CAMERA_H
 #define GLM_ENABLE_EXPERIMENTAL
