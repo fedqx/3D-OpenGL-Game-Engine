75edef601453dcf718d8f7092019836b8ece9da4
Fatih Melih Özgün <fedqx_403@outlook.com>
2018-08-25 17:25:33 +0300

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -32,7 +32,7 @@ bool Engine::init()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	window = glfwCreateWindow(1355, 697, "Frozen Circuit Engine", NULL, NULL);
+	window = glfwCreateWindow(1355, 697, "FCE", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
